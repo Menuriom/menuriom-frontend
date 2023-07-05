a94920693dad80cbefac7600df03e5d11bef6dd1
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2023-07-05 19:56:38 +0330

    fixed qr code custom image gradient issue

--- pages/panel/[brandID]/menu/qr-code/index.vue
+++ pages/panel/[brandID]/menu/qr-code/index.vue
@@ -13,22 +13,26 @@
 import QR from "~/composables/qrcodegen";
 import { h } from "vue";
 
+// ====================================================
+
 const ErrorCorrectLevelMap = {
     L: QR.QrCode.Ecc.LOW,
     M: QR.QrCode.Ecc.MEDIUM,
     Q: QR.QrCode.Ecc.QUARTILE,
     H: QR.QrCode.Ecc.HIGH,
 };
+const level = "H";
+const value = "https://menuriom.com/r/64490245ee8237588e025c8b/";
+const margin = 4;
+const size = 512;
+const background = "#373737";
+const foreground = "#f8f1cd";
+const foreground2 = "#fe3f5f";
+const dotStyle = "image";
 
-const SUPPORTS_PATH2D = () => {
-    try {
-        new Path2D().addPath(new Path2D());
-    } catch (e) {
-        return false;
-    }
-    return true;
-};
+const numCells = ref(0);
 
+// SVG method -------------------------------------------------
 const generatePath = (modules, margin = 0) => {
     const ops = [];
     modules.forEach(function (row, y) {
@@ -39,43 +43,17 @@ const generatePath = (modules, margin = 0) => {
                 start = null;
                 return;
             }
-
             if (x === row.length - 1) {
                 if (!cell) return;
-
                 if (start === null) ops.push(`M${x + margin},${y + margin} h1v1H${x + margin}z`);
                 else ops.push(`M${start + margin},${y + margin} h${x + 1 - start}v1H${start + margin}z`);
-
                 return;
             }
-
             if (cell && start === null) start = x;
         });
     });
     return ops.join("");
 };
-
-const loadImage = (src) => {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.src = src;
-        img.onload = resolve(img);
-        img.onerror = (e) => reject(e);
-    });
-};
-
-// ====================================================
-
-const value = "https://menuriom.com/r/64490245ee8237588e025c8b/";
-const level = "H";
-const margin = 4;
-const size = 512;
-const background = "#fff";
-const foreground = "#000";
-const dotStyle = "image";
-
-const numCells = ref(0);
-const fgPath = ref("");
 const generateSVG = () => {
     const cells = QR.QrCode.encodeText(value, ErrorCorrectLevelMap[level]).getModules();
     numCells.value = cells.length + margin * 2;
@@ -84,7 +62,7 @@ const generateSVG = () => {
     const imageSize = 12;
     const pos = numCells.value / 2 - imageSize / 2;
 
-    fgPath.value = generatePath(cells, margin);
+    const fgPath = generatePath(cells, margin);
 
     const props = {
         width: size,
@@ -97,15 +75,16 @@ const generateSVG = () => {
     return h("svg", props, [
         // ...
         h("path", { fill: background, d: `M0,0 h${numCells.value}v${numCells.value}H0z` }),
-        h("path", { fill: foreground, d: fgPath.value }),
+        h("path", { fill: foreground, d: fgPath }),
         h("rect", { x: pos, y: pos, height: imageSize, width: imageSize, fill: background, rx: "2" }),
         h("image", { href: "/avatar.webp", x: pos, y: pos, height: imageSize, width: imageSize }),
     ]);
 };
+// -------------------------------------------------
 
 const canvasEl = ref(); // Dom Ref
 const generateCanvas = (customDot) => {
-    const radiuos = 0.55;
+    const radius = 0.55;
     const canvas = canvasEl.value;
     const ctx = canvas.getContext("2d");
 
@@ -118,60 +97,35 @@ const generateCanvas = (customDot) => {
     ctx.scale(scale, scale);
 
     const grd = ctx.createLinearGradient(0, numCells.value / 2, numCells.value / 2, numCells.value);
-    grd.addColorStop(0, "#3871cd");
-    grd.addColorStop(1, "#2e2f5f");
+    grd.addColorStop(0, foreground);
+    grd.addColorStop(1, foreground2);
 
-    ctx.fillStyle = background;
-    ctx.fillRect(0, 0, numCells.value, numCells.value);
-
-    ctx.fillStyle = grd || foreground;
-    ctx.strokeStyle = grd || foreground;
-    // if (SUPPORTS_PATH2D()) {
-    if (0) {
-        ctx.fill(new Path2D(generatePath(cells, margin)));
-    } else {
-        console.log({ customDot });
-        if (customDot && dotStyle === "image") {
-            cells.forEach((row, rdx) => {
-                row.forEach((cell, cdx) => {
-                    if (!cell) return;
-                    ctx.drawImage(customDot, cdx + margin, rdx + margin, 1, 1);
-                });
+    if (customDot && dotStyle === "image") {
+        cells.forEach((row, rdx) => {
+            row.forEach((cell, cdx) => {
+                if (!cell) return;
+                // TODO : redius image paddings
+                ctx.drawImage(customDot, cdx + margin, rdx + margin, 1.2, 1.2);
             });
-        }
+        });
+    } else {
         cells.forEach(function (row, rdx) {
             row.forEach(function (cell, cdx) {
+                // TODO : replace all by images
                 if (!cell) return;
-                ctx.beginPath();
+                const newRadius = radius * Math.max(Math.random(), 0.6); // random radius
                 switch (dotStyle) {
                     case "square":
                         ctx.fillRect(cdx + margin, rdx + margin, 1.05, 1.05);
                         break;
                     case "square-dot":
-                        ctx.fillRect(cdx + margin, rdx + margin, radiuos * 1.3, radiuos * 1.3);
-                        break;
-                    case "triangle":
-                        ctx.ellipse(cdx + margin + radiuos, rdx + margin + radiuos, radiuos, radiuos, Math.PI / 4, 4.7, 2 * Math.PI);
-                        ctx.stroke();
-                        break;
-                    case "half-moon":
-                        ctx.ellipse(cdx + margin + radiuos, rdx + margin + radiuos, radiuos, radiuos, Math.PI / 4, 0, 1.2 * Math.PI);
-                        ctx.fill();
-                        break;
-                    case "half-moon2":
-                        ctx.ellipse(cdx + margin + radiuos, rdx + margin + radiuos, radiuos, radiuos, Math.PI / 4, 3, 2 * Math.PI);
-                        ctx.fill();
-                        break;
-                    case "pins":
-                        ctx.ellipse(cdx + margin + radiuos, rdx + margin + radiuos, radiuos / 2.5, radiuos / 2.5, Math.PI / 1, 4, 2 * Math.PI);
-                        ctx.stroke();
-                        break;
-                    case "pins2":
-                        ctx.ellipse(cdx + margin + radiuos, rdx + margin + radiuos, radiuos / 2.5, radiuos / 2.5, Math.PI / 1, 2, 1.25 * Math.PI);
-                        ctx.stroke();
+                        ctx.fillRect(cdx + margin, rdx + margin, radius * 1.3, radius * 1.3);
+                        // ctx.fillRect(cdx + margin, rdx + margin, newRadius * 2, newRadius * 2);
                         break;
                     case "dot":
-                        ctx.arc(cdx + margin + radiuos, rdx + margin + radiuos, radiuos, radiuos, 2 * Math.PI);
+                        ctx.beginPath();
+                        ctx.arc(cdx + margin + radius, rdx + margin + radius, radius * 0.9, radius * 0.9, 2 * Math.PI);
+                        // ctx.arc(cdx + margin + radius, rdx + margin + radius, newRadius, newRadius, 2 * Math.PI);
                         ctx.fill();
                         break;
                 }
@@ -179,7 +133,16 @@ const generateCanvas = (customDot) => {
         });
     }
 
-    customCorners(ctx, radiuos);
+    ctx.globalCompositeOperation = "source-in";
+    ctx.fillStyle = grd || foreground;
+    ctx.fillRect(margin, margin, cells.length, cells.length);
+
+    ctx.globalCompositeOperation = "destination-over";
+    ctx.fillStyle = background;
+    ctx.fillRect(0, 0, numCells.value, numCells.value);
+
+    ctx.globalCompositeOperation = "source-over";
+    customCorners(ctx, radius);
     setLogo(ctx);
 };
 const setLogo = (ctx) => {
@@ -202,6 +165,7 @@ const setLogo = (ctx) => {
     ctx.roundRect(pos, pos, imageSize, imageSize, Math.max(borderRadius - 1, 0));
     ctx.fill();
 
+    ctx.save();
     ctx.clip();
     const img = new Image();
     // img.src = "/file/logos/d7af7f85-bbd4-42f7-b067-733273217f9c.webp";
@@ -211,7 +175,7 @@ const setLogo = (ctx) => {
     img.onload = () => ctx.drawImage(img, pos, pos, imageSize, imageSize);
     ctx.restore();
 };
-const customCorners = (ctx, radiuos) => {
+const customCorners = (ctx, radius) => {
     const offset = margin - 0.2;
     ctx.fillStyle = background;
     ctx.beginPath();
@@ -220,27 +184,30 @@ const customCorners = (ctx, radiuos) => {
     ctx.rect(0 + offset, numCells.value - (8 + offset), 8, 8.2);
     ctx.fill();
 
-    ctx.strokeStyle = "#454545";
-    const cornersRadius = 1;
+    ctx.strokeStyle = "#fff4dd";
+    const cornersRadius = 10;
     ctx.beginPath();
-    ctx.roundRect(0 + margin + radiuos, 0 + margin + radiuos, 6, 6, cornersRadius);
-    ctx.roundRect(numCells.value - (6 + margin + radiuos), 0 + margin + radiuos, 6, 6, cornersRadius);
-    ctx.roundRect(0 + margin + radiuos, numCells.value - (6 + margin + radiuos), 6, 6, cornersRadius);
+    ctx.roundRect(0 + margin + radius, 0 + margin + radius, 6, 6, cornersRadius);
+    ctx.roundRect(numCells.value - (6 + margin + radius), 0 + margin + radius, 6, 6, cornersRadius);
+    ctx.roundRect(0 + margin + radius, numCells.value - (6 + margin + radius), 6, 6, cornersRadius);
     ctx.stroke();
 
-    ctx.fillStyle = "#9f74cd";
+    ctx.fillStyle = "#cf94ad";
     const cornersCenterRadius = 1;
     ctx.beginPath();
-    ctx.roundRect(1.5 + margin + radiuos, 1.5 + margin + radiuos, 3, 3, cornersCenterRadius);
-    ctx.roundRect(numCells.value - (4.5 + margin + +radiuos), 1.5 + margin + radiuos, 3, 3, cornersCenterRadius);
-    ctx.roundRect(1.5 + margin + radiuos, numCells.value - (4.5 + margin + radiuos), 3, 3, cornersCenterRadius);
+    ctx.roundRect(1.5 + margin + radius, 1.5 + margin + radius, 3, 3, cornersCenterRadius);
+    ctx.roundRect(numCells.value - (4.5 + margin + +radius), 1.5 + margin + radius, 3, 3, cornersCenterRadius);
+    ctx.roundRect(1.5 + margin + radius, numCells.value - (4.5 + margin + radius), 3, 3, cornersCenterRadius);
     ctx.fill();
 };
 
 onMounted(async () => {
-    // TODO : customDot images not loading currectly
-    const img = await loadImage("/icons/code.svg");
-    generateCanvas(img);
+    const img = new Image();
+    img.src = "/icons/qr-dots/Radio button checked.svg";
+
+    img.onload = () => {
+        generateCanvas(img);
+    };
 });
 
 const saveCanvas = () => {
