c95160dad927deeafce1c6e1d5f25e23f31efb96
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2023-11-04 18:31:34 +0330

    csrf req update

--- server/csrf.js
+++ server/csrf.js
@@ -1,9 +1,9 @@
 export const checkCsrf = (event, xsrfCookie) => {
-    const { req } = event;
+    const { req } = event.node;
 
     // in GET method we don't need csrf checks
     if (req.method == "GET") return true;
     if (req.headers["x-xsrf-token"] === xsrfCookie) return true;
-    
+
     return false;
 };
